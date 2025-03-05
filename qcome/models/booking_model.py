b5ec197c3a36a417fc34910fefac08843e0f4d8d
partha <partha.sutradharmy@gmail.com>
2025-03-05 21:18:36 +0530

    Add Booking model and related URL and view; remove request model

---
+++ qcome/models/booking_model.py
@@ -0,0 +1,34 @@
+from django.db import models
+from ..constants import Vichel_Type
+from django.db.models import Q, UniqueConstraint
+
+class Booking(models.Model):
+    customer = models.ForeignKey('User', on_delete=models.CASCADE, related_name='fk_user_bookings_users_id')
+    vichel_type = models.IntegerField(
+        choices=[(v_type.value, v_type.name) for v_type in Vichel_Type],
+        blank=False, null=False
+    )
+    current_location = models.CharField(max_length=255, null=False, blank=False)
+    problem = models.CharField(max_length=250, null=False, blank=False)
+
+
+    is_active = models.BooleanField(default=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_created=True)
+    created_by = models.ForeignKey('User', on_delete=models.CASCADE, related_name='fk_user_bookings_created_users_id')
+    updated_by = models.ForeignKey('User', on_delete=models.CASCADE, related_name='fk_user_bokkings_updated_users_id')
+
+
+    class Meta:
+        db_table = 'bookings'
+        constraints =[
+            UniqueConstraint(
+            fields=['user'],
+            condition=Q(is_active=True),
+            name='unique_active_booking_per_user'
+            )
+        ]
+
+
+    def __str__(self):
+        return f"ID: {self.id}, Created at: {self.created_at}, Active: {self.is_active}"
