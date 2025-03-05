13f639624a50578b8a58944e9c54d8f31a7a8059
partha <partha.sutradharmy@gmail.com>
2025-03-05 19:34:51 +0530

    set-up

--- qcome/models/user_model.py
+++ qcome/models/user_model.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    username = None  # Completely remove username field
     first_name = models.CharField(max_length=50)
     middle_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
+    
 
     is_active = models.BooleanField(default=True, blank=True)
     is_staff = models.BooleanField(default=False, blank=True)
