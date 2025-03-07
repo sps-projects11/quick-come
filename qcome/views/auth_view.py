a33de906a63d15ca057f554e6493f7e521893c1b
subham <modaksuham866@gmail.com>
2025-03-07 19:08:50 +0530

    "add functionality"

--- qcome/views/auth_view.py
+++ qcome/views/auth_view.py
@@ -1,6 +1,6 @@
 from django.views import View
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout
 from django.http import JsonResponse
 from django.core.mail import send_mail
 from django.conf import settings
@@ -56,6 +56,8 @@ class RequestOTPView(View):
         email = request.GET.get("email")
         if not email:
             return JsonResponse({"status": "error", "message": "Email required."})
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({"status": "error", "message": "Email is already registered."})
 
         otp = random.randint(100000, 999999)
         OTP_STORAGE[email] = {"otp": otp, "verified": False, "timestamp": time.time()}
