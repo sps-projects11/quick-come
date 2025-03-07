44a0bbb814170847859215a02978bf7506152c02
shubhankarchdas <shubhankarchdas@gmail.com>
2025-03-07 22:04:30 +0530

    Home page navber footer for all page done (Add carousel feature to home page and refactor CSS styles)

---
+++ static/js/home.js
@@ -0,0 +1,37 @@
+// carousal
+    document.addEventListener('DOMContentLoaded', function () {
+        var swiper = new Swiper(".swiper", {
+            loop: true,
+            autoplay: {
+                delay: 9000,
+                disableOnInteraction: false,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    });
+
+// mobile menu
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navber');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+
