a7508a184e8ac3861c6071545122f0ebd28c99fd
partha <partha.sutradharmy@gmail.com>
2025-03-08 17:46:41 +0530

    Add admin dashboard and profile management features with success and error messages

--- qcome/views/admin_view.py
+++ qcome/views/admin_view.py
@@ -4,6 +4,9 @@ from ..decorators import auth_required, role_required
 from ..constants import Role
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages  # For user feedback
+import datetime
+from ..constants import success_message
+from ..constants import error_message
 
 class LoginAdminView(View):
     def get(self, request):
@@ -38,11 +41,38 @@ class LoginOutAdminView(View):
 @role_required(Role.ADMIN.value, Role.SUPER_ADMIN.value, page_type='admin')
 class AdminHomeView(View):
     def get(self, request):
-        return render(request, 'adminuser/home/index.html')
+        user = request.user
+        return render(request, 'adminuser/home/index.html',{user:'user'})
 
     
 @auth_required
 class AsminDashboard(View):
     def get(self, request):
+        return render(request, 'adminuser/dashboard.html')
+    
+
+class AdminProfileView(View):
+    def get(self, request):
+        return render(request, 'adminuser/profile/profile.html')
+
+class AdminPasswordUpdateView(View):
+    def get(self, request):
         return
     
+
+class AdminProfileUpdateView(View):
+    def get(self, request):
+        return render(request, 'adminuser/profile/update_profile.html')
+
+    def post(self, request):
+        user = request.user
+        first_name = request.POST.get('first_name', '').strip()
+        middle_name = request.POST.get('middle_name', '').strip()
+        last_name = request.POST.get('last_name', '').strip()
+        email = request.POST.get('email', '').strip()
+        phone = request.POST.get('phone', '').strip()
+        gender = request.POST.get('gender', '').strip()
+        dob = request.POST.get('dob', '').strip()
+
+        messages.success(request, "Profile updated successfully.")
+        return redirect('myadmin_profile')
