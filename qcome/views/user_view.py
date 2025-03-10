6c295bdfe6283d47fdc2f86599ebf89de593fe08
Badhan Sarkar <badhansarkar164@gmail.com>
2025-03-10 11:11:56 +0530

    upodate again

--- qcome/views/user_view.py
+++ qcome/views/user_view.py
@@ -1,9 +1,10 @@
 from django.views import View
 from qcome.services import user_service
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from ..decorators import auth_required, role_required
 from ..constants import Role
-from ..services import get_user_details
+from ..services import get_user_details,update_user_details
+
 
 @auth_required(login_url='/sign-in/')
 @role_required(Role.END_USER.value, page_type='enduser')
@@ -28,9 +29,27 @@ class EnduserProfileUpdate(View):
         context = {'user_details': user_details}
         return render(request, 'enduser/profile/user_profile_update.html', context)
 
+    def post(self, request, user_id):
+        user = get_user_details(user_id)
+        if user:
+            update_user_details(user, request.POST, request.FILES)
+            return redirect('user_profile')  
+        return redirect('user_profile')  
+
+
 
 class EnduserProfileDelete(View):
-    def post(self, request):
-        return
+    def get(self, request, user_id):
+        user_del = get_user_details(user_id)
+        context = {'user_del': user_del}
+        return render(request, 'enduser/profile/user_profile_delete.html', context)
+
+    def post(self, request, user_id):
+        user = get_user_details(user_id)
+        if user:
+            user.delete()
+            return redirect('user_profile')  
+        return redirect('user_profile')
+  
     
 #user_profile_update.html
