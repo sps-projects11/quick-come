49a895e2997b69d5495f28f341c554197fcf76f2
partha <partha.sutradharmy@gmail.com>
2025-03-11 01:05:05 +0530

    Add service creation functionality and enhance service catalog UI

--- qcome/services/service_service.py
+++ qcome/services/service_service.py
@@ -2,3 +2,12 @@ from ..models import User, ServiceCatalog
 
 def service_List():
     return ServiceCatalog.objects.filter(is_active=True).all()
+
+def service_create(user,service_name, service_image, spare_part, price):
+    ServiceCatalog.objects.create(
+        service_name = service_name,
+        service_image = service_image,
+        spare_part = spare_part,
+        price = price,
+        created_by = user,
+    )
