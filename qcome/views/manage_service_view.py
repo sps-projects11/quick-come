e5a1d601ff58e98137141d509a571e1a1383cc8a
partha <partha.sutradharmy@gmail.com>
2025-03-11 01:14:46 +0530

    Refactor service image upload path and improve handling of spare part input

--- qcome/views/manage_service_view.py
+++ qcome/views/manage_service_view.py
@@ -21,18 +21,18 @@ class ManageServiceListCreate(View):
         user = request.user
         if user.roles in (Role.ADMIN.value, Role.SUPER_ADMIN.value):
             service_name = request.POST.get('service_name', '')
-            spare_part = request.POST.get('spare_part', '')
+            spare_part = request.POST.get('spare_part') or None
             price = request.POST.get('price', '')
             
             service_image_file = request.FILES.get('service_image')
             service_image_path = ''  # Default to empty if no file is provided
             
             if service_image_file:
-                # Define the static directory path where you want to save the image
-                static_upload_dir = os.path.join(settings.BASE_DIR, 'static', 'all-Pictures', 'uploads')
+                # Define the static directory path where you want to save the image in your service folder
+                static_service_catalog_img_dir = os.path.join(settings.BASE_DIR, 'static', 'all-Pictures', 'service_catalog_img')
                 # Create the directory if it doesn't exist
-                if not os.path.exists(static_upload_dir):
-                    os.makedirs(static_upload_dir)
+                if not os.path.exists(static_service_catalog_img_dir):
+                    os.makedirs(static_service_catalog_img_dir)
                 
                 # Compute a hash of the file's content
                 md5_hash = hashlib.md5()
@@ -43,7 +43,7 @@ class ManageServiceListCreate(View):
                 # Get the original file extension (e.g. .jpg, .png)
                 _, ext = os.path.splitext(service_image_file.name)
                 new_file_name = f"{file_hash}{ext}"
-                file_path = os.path.join(static_upload_dir, new_file_name)
+                file_path = os.path.join(static_service_catalog_img_dir, new_file_name)
                 
                 # Check if a file with this hash already exists
                 if not os.path.exists(file_path):
@@ -54,8 +54,8 @@ class ManageServiceListCreate(View):
                         for chunk in service_image_file.chunks():
                             destination.write(chunk)
                 
-                # Set the URL for the saved image (using static URL)
-                service_image_path = f'/static/all-Pictures/uploads/{new_file_name}'
+                # Set the URL for the saved image (using the static URL pointing to the service folder)
+                service_image_path = f'/static/all-Pictures/service_catalog_img/{new_file_name}'
             
             # Save the service record using your service_create helper function
             service_service.service_create(user, service_name, service_image_path, spare_part, price)
