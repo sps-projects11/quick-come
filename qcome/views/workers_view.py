0738f1a0436d4ba3bfd3faf550e216728ee5bcda
Badhan Sarkar <badhansarkar164@gmail.com>
2025-03-10 20:18:18 +0530

    Complete the profile

--- qcome/views/workers_view.py
+++ qcome/views/workers_view.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from ..services import get_all_workers, get_worker_by_id, create_worker, update_worker, delete_worker
+from ..services import *
+from qcome.models import User, Garage
 
 class Worker(View):
     def get(self, request):
@@ -8,9 +9,11 @@ class Worker(View):
         return render(request, 'workers/workers_profile.html', {'workers': workers})
 
     def post(self, request):
+        worker_instance = get_object_or_404(User, id=int(request.POST['worker']))
+        garage_instance = get_object_or_404(Garage, id=int(request.POST['garage']))
         data = {
-            'worker': request.POST['worker'],
-            'garage': request.POST['garage'],
+            'worker': worker_instance,
+            'garage': garage_instance,
             'experience': request.POST['experience'],
             'expertise': request.POST['expertise'],
             'is_verified': request.POST.get('is_verified', False),
@@ -22,31 +25,35 @@ class Worker(View):
 class WorkerUpdate(View):
     def get(self, request, worker_id):
         worker = get_worker_by_id(worker_id)
-        if worker is None:
-            return redirect('worker_list')  # Redirect if worker does not exist
+        # if worker is None:
+        #     return redirect('workers')  # Redirect if worker does not exist
         return render(request, 'workers/workers_profile_update.html', {'worker': worker})
 
     def post(self, request, worker_id):
+        worker_instance = get_object_or_404(User, id=int(request.POST['worker']))
+        garage_instance = get_object_or_404(Garage, id=int(request.POST['garage']))
         data = {
-            'worker': request.POST['worker'],
-            'garage': request.POST['garage'],
+            'worker': worker_instance,
+            'garage': garage_instance,
             'experience': request.POST['experience'],
             'expertise': request.POST['expertise'],
             'is_verified': request.POST.get('is_verified', False),
             'is_active': request.POST.get('is_active', True),
         }
-        if update_worker(worker_id, data) is None:
-            return redirect('worker_list')  # Redirect if worker does not exist
-        return redirect('workers')
+        # if update_worker(worker_id, data) is None:
+        #     return redirect('workers')  # Redirect if worker does not exist
+        # return redirect('workers')
+        return render(request, 'workers/workers_profile_update.html',data)
 
 class WorkerDelete(View):
     def get(self, request, worker_id):
         worker = get_worker_by_id(worker_id)
-        if worker is None:
-            return redirect('worker_list')  # Redirect if worker does not exist
+        # if worker is None:
+        #     return redirect('workers')  # Redirect if worker does not exist
         return render(request, 'workers/workers_profile_delete.html', {'worker': worker})
 
     def post(self, request, worker_id):
-        if not delete_worker(worker_id):
-            return redirect('worker_list')  # Redirect if worker does not exist
-        return redirect('workers')
+        # if not delete_worker(worker_id):
+        #     return redirect('workers')  # Redirect if worker does not exist
+        # return redirect('workers')
+        return render(request, 'workers/workers_profile_delete.html')
