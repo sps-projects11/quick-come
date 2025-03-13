0550b43fada29d1e4638045f55147daceafca2ae
partha <partha.sutradharmy@gmail.com>
2025-03-13 16:16:43 +0530

    Update user profile display and enhance OTP request handling; refactor booking model for multiple services and add user list template

--- qcome/models/booking_model.py
+++ qcome/models/booking_model.py
@@ -1,6 +1,7 @@
 from django.db import models
 from ..constants import Vehicle_Type
 from django.db.models import Q, UniqueConstraint
+from django.contrib.postgres.fields import ArrayField
 
 class Booking(models.Model):
     customer = models.ForeignKey('User', on_delete=models.CASCADE, related_name='fk_user_bookings_users_id')
@@ -8,13 +9,13 @@ class Booking(models.Model):
         choices=[(v_type.value, v_type.name) for v_type in Vehicle_Type],
     )
     current_location = models.CharField(max_length=255)
-    service = models.ForeignKey('ServiceCatalog', on_delete=models.CASCADE, related_name='fk_service_booking_services_id')
+    service = models.ManyToManyField('ServiceCatalog', related_name='fk_service_booking_services_id')
     description = models.CharField(blank=True,null=True,max_length=255)
     assigned_worker = models.ForeignKey('Worker', on_delete=models.CASCADE, null=True, blank=True, related_name='fk_worker_booking_workers_id')
 
     is_active = models.BooleanField(default=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_created=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
     created_by = models.ForeignKey('User', on_delete=models.CASCADE, related_name='fk_user_bookings_created_users_id', blank=True, null=False)
     updated_by = models.ForeignKey('User', on_delete=models.CASCADE, related_name='fk_user_bookings_updated_users_id', null=True, blank=True)
 
