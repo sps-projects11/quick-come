8dbe7a74ea0df5f6445f2ffd918eef6c08f462a0
shubhankarchdas <shubhankarchdas8@gmail.com>
2025-03-14 13:26:47 +0530

    Add garage bookings feature with booking details display and navigation updates

--- qcome/services/garage_service.py
+++ qcome/services/garage_service.py
@@ -1,4 +1,24 @@
 from ..models import Garage
+from ..models import Booking
+
+def get_garage_bookings(garage_owner):
+    """
+    Get all bookings for the garage owned by the given user.
+    """
+    garage = Garage.objects.filter(garage_owner=garage_owner).first()
+    if not garage:
+        return None
+    return Booking.objects.filter(is_active=True).order_by('-created_at')
+
+def get_booking_details(booking_id):
+    """
+    Get a single booking detail.
+    """
+    return Booking.objects.get(id=booking_id)
+
+
+
+
 
 def get_garage_list():
     garages = Garage.objects.all()
--- qcome/views/garage_view.py
+++ qcome/views/garage_view.py
@@ -1,34 +1,19 @@
 from django.shortcuts import render
 from django.views import View
-
-# class GarageHomeView(View):
-#     def get(self, request):
-#         navbar_labels = {
-#             "home": "Garage Home",
-#             "booking": "Garage Bookings",
-#             "contact": "Garage Worker",
-#             "blog": "Payment",
-#         }
-
-#         navbar_urls = {
-#             "home": "/garage/",
-#             "booking": "/garage/booking/",
-#             "contact": "/garage/workers/",
-#             "blog": "/garage/payment/",
-#         }
-
-#         return render(request, 'garage/index.html', {
-#             'navbar_labels': navbar_labels,
-#             'navbar_urls': navbar_urls,
-#             'is_garage_page': True  # Flag to check if it's a garage page
-#         })
-
+from ..services import get_garage_bookings
 
 
 
 class GarageCreateView(View):
     def get(self, request):
-        return
+        user = request.user
+        bookings = get_garage_bookings(user)
+
+        if bookings is None:
+            return render(request, 'garage/bookings.html', {'error': 'No garage found for this user.'})
+
+        return render(request, 'garage/bookings.html', {'bookings': bookings})
+
     def post(self, request):
         return
 
