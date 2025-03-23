085b22cb2069dbb7444b1841f9e66bfa0ce2ff96
partha <partha.sutradharmy@gmail.com>
2025-03-23 22:11:51 +0530

    Enhance admin dashboard: display admin's first name and pass admin data to various views for improved context

--- qcome/views/manage_payment_view.py
+++ qcome/views/manage_payment_view.py
@@ -1,11 +1,12 @@
 from django.views import View
 from django.shortcuts import render
-from qcome.services import payment_service
+from qcome.services import payment_service, user_service
 
 
 class ManagePaymentListView(View):
     def get(self, request):
+        admin_data = user_service.get_user(request.user.id)
         payments = payment_service.get_all_payments()
-        return render(request, 'adminuser/payments/payment_list.html', { 'payments_data': payments })
+        return render(request, 'adminuser/payments/payment_list.html', { 'payments_data': payments, 'admin': admin_data})
     
 
