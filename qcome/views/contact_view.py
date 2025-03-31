65db49c7e11d3bd527dcb6a16f575b1d28440304
partha <partha.sutradharmy@gmail.com>
2025-03-31 21:03:08 +0530

    Refactor contact view to simplify rendering logic for end users

--- qcome/views/contact_view.py
+++ qcome/views/contact_view.py
@@ -4,16 +4,9 @@ from django.core.mail import EmailMultiAlternatives
 from django.contrib import messages
 from ..constants.error_message import ErrorMessage
 from ..constants.success_message import SuccessMessage
-from qcome.services import workers_service, garage_service
 
 class ContactView(View):
     def get(self, request):
-        is_worker = workers_service.is_user_a_garage_worker(request.user.id)
-        is_garage = garage_service.is_user_a_garage_owner(request.user.id)
-        if is_worker:
-            return render(request, 'worker/Contact/index.html')
-        elif is_garage:
-            return render(request, 'garage/Contact/index.html')
         return render(request, 'enduser/Contact/index.html')
     
     def post(self, request):
