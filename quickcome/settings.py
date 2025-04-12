047c66a261dc81a15965e48e53a3f0bae5f2c504
Subham <modaksubham866@gmail.com>
2025-04-12 14:52:17 +0530

    "add database"

--- quickcome/settings.py
+++ quickcome/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from env_config import env
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'quickcome.urls'
@@ -75,14 +77,7 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': env('DATABASE_PORT'),
-    }
+    'default': dj_database_url.config(default=env('DATABASE_URL'))
 }
 
 
@@ -141,6 +136,10 @@ EMAIL_HOST_USER = "sps.projects1010@gmail.com"
 EMAIL_HOST_PASSWORD = "vies eimx rzrv geik"  # Use the generated password here
 SITE_URL = "http://127.0.0.1:8000"  # Change this to your actual domain
 DEFAULT_FROM_EMAIL = 'sps.projects1010@gmail.com'
+
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
