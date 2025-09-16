c7a01eda96328dbf284bdcbeb6b311e09a423303
subham modak <modaksubham866@gmail.com>
2025-09-16 12:38:28 +0530

    "update cores"

--- quickcome/settings.py
+++ quickcome/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
-CORS_ALLOWED_ORIGINS = ["http://localhost:8000"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:8000","https://quick-come.onrender.com"]
 
 # Application definition
 
